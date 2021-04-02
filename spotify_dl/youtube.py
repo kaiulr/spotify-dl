6db5744b0a21f21e588ef8f13fe5f2ea2ad81f84
kaiulr <81491586+kaiulr@users.noreply.github.com>
2021-04-02 05:51:59 +0530

    Update youtube.py

--- spotify_dl/youtube.py
+++ spotify_dl/youtube.py
@@ -21,7 +21,7 @@ def download_songs(songs, download_directory, format_string, skip_mp3, keep_play
     """
     log.debug(f"Downloading to {download_directory}")
     for song in songs:
-        query = f"{song.get('artist')} - {song.get('name')} Lyrics".replace(":", "").replace("\"", "")
+        query = f"{song.get('artist')} - {song.get('name')} "provided to youtube by"".replace(":", "").replace("\"", "")
         download_archive = path.join(download_directory, 'downloaded_songs.txt')
 
         file_name = sanitize(f"{song.get('artist')} - {song.get('name')}", '#')  # youtube-dl automatically replaces with #
